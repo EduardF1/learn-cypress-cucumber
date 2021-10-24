bca058b167847efee4e742efc7bf5a332a47bbac
EduardF1 <fischer_eduard@yahoo.com>
2021-10-24 22:03:06 +0200

    add some tests

--- todomvc-typescript-react/cypress/integration/homepage/steps.js
+++ todomvc-typescript-react/cypress/integration/homepage/steps.js
@@ -1,14 +1,20 @@
-import {Given, Then, When} from 'cypress-cucumber-preprocessor/steps';
-
-Given('I navigate to the home page', () => {
-  cy.visit('/');
-});
-
-When('the home page has loaded', () => {
-  cy.get('.todoapp').should('be.visible');
-});
+import {Then} from 'cypress-cucumber-preprocessor/steps';
 
 Then('I see the home page', () => {
   cy.get('.header').should('be.visible');
   cy.get('.info').should('be.visible');
+});
+
+Given('I have the following todos:', (dataTable) => {
+  let todos = [];
+  dataTable.rawTable.slice(1).forEach((todo, index) => {
+    const [title, completed] = todo;
+    todos.push({
+      id: `test-todo-{index}`,
+      title,
+      completed: completed === 'true'
+    });
+  });
+  cy.visit('/');
+  window.localStorage.setItem('react-todos', JSON.stringify(todos));
 });
