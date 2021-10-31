79896a35ab393133cc96b5a3821a72b20e5292a7
EduardF1 <fischer_eduard@yahoo.com>
2021-10-31 11:15:33 +0100

    add edit a todo

---
+++ todomvc-typescript-react/cypress/integration/edit-a-todos-title/when-i-choose-to-edit-string.js
@@ -0,0 +1,7 @@
+import {When} from "cypress-cucumber-preprocessor/steps";
+
+When("I choose to edit {string}", (title) => {
+  cy.get(".todo-list li .view label")
+    .contains(title)
+    .dblclick();
+});
